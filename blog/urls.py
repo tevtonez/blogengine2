242b486b86acb8884cd2e33818923de0523c2e0f
tevtonez <tevtonez@gmail.com>
2017-09-04 23:47:11 +0200

    add Blog CRUD class-based views, urls

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from blog import views
+
+
+app_name = 'blog'
+urlpatterns = [
+  url(r'^$', views.PostListView.as_view(), name = 'post_list'),
+  url(r'^about/$', views.AboutView.as_view(), name = 'about'),
+  url(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name = 'post_detail'),
+  url(r'^post/new/$', views.CreatePost.as_view(), name = 'post_new'),
+  url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name = 'post_edit'),
+  url(r'^post/(?P<pk>\d+)/delete/$', views.PostDeleteView.as_view(), name = 'post_delete'),
+  url(r'^drafts/$', views.DraftsListView.as_view(), name = 'post_draft_list'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+#from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views.generic import (TemplateView,
+                                  ListView,
+                                  DetailView,
+                                  CreatelView,
+                                  UpdateView,
+                                  DeleteView
+)
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
 
-# Create your views here.
+
+
+class AboutView(TemplateView):
+  template_name = 'about.html'
+
+
+
+class PostListView(ListView)
+  model = Post
+
+  def get_queryset(self):
+    return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+
+class PostDetailView(DetailView):
+  model = Post
+
+
+
+class CreatePostView(LoginRequiredMixin, CreatelView):
+  login_url = '/login/'
+  redirect_field_name = 'blog/post_detail.html'
+  form_class = PostForm
+  model = Post
+
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+  login_url = '/login/'
+  redirect_field_name = 'blog/post_detail.html'
+  form_class = PostForm
+  model = Post
+
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+  model = Post
+  success_url = reverse_lazy('post_list')
+
+
+
+class DraftsListView(LoginRequiredMixin, ListView):
+  model = Post
+  login_url = '/login/'
+  redirect_field_name = 'blog/post_list.html'
+  def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).now.order_by('-published_date')
