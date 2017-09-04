be73601522c03123b38774fd93756e8f9a5ce058
tevtonez <tevtonez@gmail.com>
2017-09-04 23:11:00 +0200

    set up forms, static and templates folders and settings

--- blog/models.py
+++ blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 
 
-class Comments(models.Model):
+class Comment(models.Model):
   post = models.ForeignKey('blog.Post', related_name = 'comments')
   author = models.CharField(max_length = 200)
   text = models.TextField()
