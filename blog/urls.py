ca45aa3b767d0c2109e348647b7f475b88ea416b
tevtonez <tevtonez@gmail.com>
2017-09-05 15:31:44 +0200

    add the rest of comment views

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
   url(r'^post/new/$', views.CreatePostView.as_view(), name = 'post_new'),
   url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name = 'post_edit'),
   url(r'^post/(?P<pk>\d+)/delete/$', views.PostDeleteView.as_view(), name = 'post_delete'),
-  url(r'^drafts/$', views.DraftsListView.as_view(), name = 'post_draft_list'),
+  url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name = 'post_publish'),
   url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name = 'add_comment_to_post'),
+  url(r'^drafts/$', views.DraftsListView.as_view(), name = 'post_draft_list'),
+  url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name = 'comment_approve'),
+  url(r'^comment/(?P<pk>\d+)/delete/$', views.comment_remove, name = 'comment_remove'),
 ]
